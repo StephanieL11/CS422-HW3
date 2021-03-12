293dc4c50205f066468e2332104086ba31ee649b
Stephanie Lin <stephanie.lin@uconn.edu>
2021-03-11 22:11:25 -0500

    finished q2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,39 @@
+
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
+/** CLASS: Main
+ *  DESCRIPTION: Handles main program logic
+ */
 public class Main {
 
+    /** METHOD: main
+     * DESCRIPTION: program entry point
+     */
     public static void main(String[] args) {
 
-        // Header
+        // Print Header
         printHeader();
 
-        // Objects
+        // Initialize Objects
         Scanner scan = new Scanner(System.in);
         Q1 q1 = new Q1();
         Q2 q2 = new Q2();
 
-        // Q1
+        // Run Programs
         runQ1(scan, q1);
-
-        // Q2
         runQ2(scan, q2);
+
+        // Print Closer
+        System.out.println("End of Program!");
 
     }
 
+
+    /** METHOD: printHeader
+     *  DESCRIPTION: prints program info to the console
+     */
     private static void printHeader() {
         System.out.println("\nMET CS 422");
         System.out.println("Spring 2021");
@@ -27,31 +41,79 @@ public class Main {
         System.out.println("Stephanie Lin\n");
     }
 
+
+    /** METHOD: runQ1
+     *  DESCRIPTION: contains program logic for assignment question 1
+     * @param scan = scanner object
+     * @param q1 = Q1 object
+     */
     private static void runQ1(Scanner scan, Q1 q1) {
+
+        // Print Header
         System.out.println("-----------------------------------------");
         System.out.println("Q1: COUNT THE DUPLICATE WORDS");
         System.out.println("-----------------------------------------");
 
+        // Prompt User to Input Sentence
         System.out.println("Please enter a sentence: ");
         String input = scan.nextLine();
+
+        // Continue Looping until User Enters Q
         while (!input.equalsIgnoreCase("q")) {
+            // Count Duplicate Words & Print Result
             int duplicateWords = q1.countDuplicateWords(input);
             System.out.println("Number of duplicate words: " + duplicateWords + "\n");
+            // Prompt User to Input Sentence, or Q to Quit
             System.out.println("Enter another sentence, or q to quit:");
             input = scan.nextLine();
         }
     }
 
+
+    /** METHOD: runQ2
+     *  DESCRIPTION: contains program logic for assignment question 2
+     * @param scan = scanner object
+     * @param q2 = Q2 object
+     */
     private static void runQ2(Scanner scan, Q2 q2) {
+
+        // Print Header
         System.out.println("-----------------------------------------");
         System.out.println("Q2: LINKED LIST w/ 25 RANDOM INTS");
         System.out.println("-----------------------------------------");
 
-        System.out.println("Print: ");
-        System.out.println("Sum: ");
-        System.out.println("Average: ");
+        // Track Whether or Not to TryAgain
+        boolean tryAgain = true;
+
+        // Continue Looping until tryAgain is false
+        while (tryAgain) {
+
+            // Create New Linked List
+            List<Integer> list = new LinkedList<>();
+            // Add 25 Random Ints
+            for (int i = 0; i < 25; i++) {
+                list.add(q2.getRandomInt());
+            }
+
+            // Print List
+            System.out.print("List: ");
+            q2.printList(list);
+            // Print Sum
+            System.out.println("Sum: " + q2.getSumList(list));
+            // Print Average
+            System.out.println("Average: " + q2.getAverageList(list));
+
+            // Prompt user to enter Y (to try again), or N (to quit)
+            System.out.println("\nTRY AGAIN? Enter Y or N.");
+            String input = scan.nextLine();
+            // Update boolean
+            if (input.equalsIgnoreCase("Y")) {
+                tryAgain = true;
+            } else {
+                tryAgain = false;
+            }
+        }
+
     }
-
-
 
 }
--- src/Q2.java
+++ src/Q2.java
@@ -1,2 +1,32 @@
+import java.util.List;
+
 public class Q2 {
+
+    /**
+     *
+     * @return a random int between 0 and 100 inclusive
+     */
+    public int getRandomInt() {
+        return (int) (Math.random() * 100);
+    }
+
+    public void printList(List<Integer> list) {
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public int getSumList(List<Integer> list) {
+        int sum = 0;
+        for (int i: list) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public float getAverageList(List<Integer> list) {
+        return (getSumList(list) / list.size());
+    }
+
 }
